c5726f57db5489b76e7be2632a34f9cb52e04ad7
devakantsanasam-png <devakant.sanasam@gmail.com>
2026-03-04 15:53:34 +0530

    Update page.js

--- dashboard/app/page.js
+++ dashboard/app/page.js
@@ -2,14 +2,12 @@
 
 import KanbanBoard from "../components/KanbanBoard";
 import Header from "../components/Header";
-import StatsBar from "../components/StatsBar";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 px-6 py-6 max-w-screen-xl mx-auto w-full">
-        <StatsBar />
         <KanbanBoard />
       </main>
     </div>
